5ebb4f0334b029779727df3d22ab7ed9fa1e0d58
mazzzystar <2680461921@qq.com>
2023-07-24 19:08:04 +0800

    init code.

---
+++ api/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template
+import requests
+import datetime
+import json
+from collections import defaultdict
+
+app = Flask(__name__)
+
+api_key = ''
+headers = {'Authorization': f'Bearer {api_key}'}
+url = 'https://api.openai.com/v1/usage'
+
+
+def get_usage(date):
+    params = {'date': date}
+    response = requests.get(url, headers=headers, params=params)
+    usage_data = response.json()['data']
+    return usage_data
+
+
+@app.route('/')
+def index():
+    date = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    usage_data = get_usage(date)
+
+    # Group usage data by timestamp and model
+    usage_by_timestamp = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
+    for data in usage_data:
+        timestamp = datetime.datetime.fromtimestamp(data['aggregation_timestamp']).strftime('%Y-%m-%d %H:%M')
+        model = data['snapshot_id']
+        context_tokens = data['n_context_tokens_total']
+        generated_tokens = data['n_generated_tokens_total']
+
+        # Calculate cost based on model
+        if model.startswith('gpt-3.5-turbo'):
+            context_cost = context_tokens / 1000 * 0.0015
+            generated_cost = generated_tokens / 1000 * 0.002
+        elif model == 'text-embedding-ada-002-v2':
+            context_cost = context_tokens / 1000 * 0.0001
+            generated_cost = 0  # No cost for generated tokens
+        else:
+            context_cost = generated_cost = 0  # Unknown model
+
+        usage_by_timestamp[timestamp][model]['context_cost'] += context_cost
+        usage_by_timestamp[timestamp][model]['generated_cost'] += generated_cost
+
+    # Prepare data for the chart
+    timestamps = sorted(usage_by_timestamp.keys())
+    model_names = sorted(set(model for usage in usage_by_timestamp.values() for model in usage.keys()))
+    datasets = []
+    for model in model_names:
+        context_costs = [usage_by_timestamp[timestamp][model]['context_cost'] for timestamp in timestamps]
+        generated_costs = [usage_by_timestamp[timestamp][model]['generated_cost'] for timestamp in timestamps]
+        total_costs = [context_costs[i] + generated_costs[i] for i in range(len(context_costs))]
+        datasets.append({'label': f'{model} (context)', 'data': context_costs})
+        datasets.append({'label': f'{model} (generated)', 'data': generated_costs})
+        datasets.append({'label': f'{model} (total)', 'data': total_costs})
+
+    return render_template('index.html', timestamps=json.dumps(timestamps), datasets=json.dumps(datasets))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
